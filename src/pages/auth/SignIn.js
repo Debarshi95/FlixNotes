3a9d2200f89b55c3ddf71bf340bbe813a14628a0
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-05 01:31:41 +0530

    update signin component folder structure

--- src/pages/auth/SignIn.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './SignIn.css';
-
-const SignIn = () => {
-  return <div>SignIn</div>;
-};
-
-export default SignIn;
---
+++ src/pages/auth/SignIn/SignIn.js
@@ -0,0 +1,70 @@
+import { Form, Formik } from 'formik';
+import React from 'react';
+import { Button, Navbar, Typography, Input } from '../../../components';
+import './SignIn.css';
+
+const SignIn = () => {
+  return (
+    <div className="Signin__root">
+      <Navbar />
+      <div className="Signin__formContainer d-flex flex-col">
+        <Typography variant="h5" className="Typography--primary my-2 text-center">
+          Signin to continue
+        </Typography>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          //   validationSchema={validateLogin()}
+          //   onSubmit={handleSubmit}
+        >
+          {({ handleSubmit: handleFormikSubmit, isSubmitting, values, errors, isValid }) => {
+            return (
+              <>
+                {errors?.error && (
+                  <Typography variant="p" className="Typography--error Typography--xs text-center">
+                    {errors.error || 'This is a error'}
+                  </Typography>
+                )}
+                <Form autoComplete="off" onSubmit={handleFormikSubmit}>
+                  <Input
+                    label="Email"
+                    name="email"
+                    type="email"
+                    hasLabel
+                    placeholder="johndoe@test.com"
+                    value={values.email}
+                  />
+                  <Input
+                    label="Password"
+                    name="password"
+                    type="password"
+                    hasLabel
+                    placeholder="******"
+                    value={values.password}
+                  />
+                  <Button
+                    component="button"
+                    type="submit"
+                    variant="contained"
+                    className="Signin__button text-bold w-10 text-10"
+                    disabled={
+                      isSubmitting ||
+                      !isValid ||
+                      Boolean(values.email === '' || values.password === '')
+                    }
+                  >
+                    {isSubmitting ? 'Submitting...' : 'Login'}
+                  </Button>
+                </Form>
+              </>
+            );
+          }}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
