5050106948f37bc0712c260068b39a4a14c17c52
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-05 13:39:46 +0530

    Signup: added form submit handler

--- src/pages/auth/Signup/Signup.js
+++ src/pages/auth/Signup/Signup.js
@@ -1,14 +1,61 @@
-import { Form, Formik } from 'formik';
 import React from 'react';
+import { AuthErrorCodes } from 'firebase/auth';
+import { Form, Formik } from 'formik';
+import { useNavigate } from 'react-router';
 import { Button, Navbar, Typography, Input } from '../../../components';
+import { checkUserNameTaken, createUser, signup } from '../../../services/firebaseApi';
+import { validateRegister } from '../../../utils/formValidations';
+import { authErrorMessage } from '../../../constants/authMessages';
 import './Signup.css';
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (values, { resetForm, setFieldError }) => {
+    const { username, email, password, confirmPassword } = values;
+    let message;
+
+    if (password !== confirmPassword) {
+      return setFieldError('confirmPassword', 'Passwords donot match');
+    }
+
+    try {
+      const usernameTaken = await checkUserNameTaken(username);
+
+      if (usernameTaken?.docs.length) {
+        message = authErrorMessage.USERNAME_TAKEN;
+      } else {
+        const res = await signup({ email, password });
+        if (res?.user) {
+          const { user } = res;
+          const doc = await createUser({ username, uid: user.uid, email });
+          if (doc?.id) {
+            navigate('/dashboard', { replace: true });
+          }
+        }
+      }
+    } catch (err) {
+      if (err?.code === AuthErrorCodes.EMAIL_EXISTS) {
+        message = authErrorMessage.EMAIL_IN_USE;
+      }
+    }
+
+    resetForm({
+      values: { ...values, password: '', confirmPassword: '' },
+      errors: { message },
+      touched: {
+        password: true,
+        confirmPassword: true,
+      },
+    });
+    return null;
+  };
+
   return (
     <div className="Signup__root">
       <Navbar />
       <div className="Signup__formContainer d-flex flex-col">
-        <Typography variant="h5" className="Typography--primary my-2 text-center">
+        <Typography variant="h5" className="Typography--primary mt-2 mb-1 text-center">
           Signup to get started
         </Typography>
         <Formik
@@ -18,15 +65,18 @@ const Signup = () => {
             password: '',
             confirmPassword: '',
           }}
-          //   validationSchema={validateLogin()}
-          //   onSubmit={handleSubmit}
+          validationSchema={validateRegister()}
+          onSubmit={handleSubmit}
         >
           {({ handleSubmit: handleFormikSubmit, isSubmitting, values, errors, isValid }) => {
             return (
               <>
-                {errors?.error && (
-                  <Typography variant="p" className="Typography--error Typography--xs text-center">
-                    {errors.error || 'This is a error'}
+                {errors?.message && (
+                  <Typography
+                    variant="p"
+                    className="Typography--error Typography--xs text-center mb-1"
+                  >
+                    {errors.message || 'Oops! Some error occurred'}
                   </Typography>
                 )}
                 <Form autoComplete="off" onSubmit={handleFormikSubmit}>
@@ -56,7 +106,7 @@ const Signup = () => {
                   />
                   <Input
                     label="Confirm Password"
-                    name="password"
+                    name="confirmPassword"
                     type="password"
                     hasLabel
                     placeholder="******"
@@ -70,10 +120,15 @@ const Signup = () => {
                     disabled={
                       isSubmitting ||
                       !isValid ||
-                      Boolean(values.email === '' || values.password === '')
+                      Boolean(
+                        values.username === '' ||
+                          values.email === '' ||
+                          values.password === '' ||
+                          values.confirmPassword === ''
+                      )
                     }
                   >
-                    {isSubmitting ? 'Submitting...' : 'Login'}
+                    {isSubmitting ? 'Submitting...' : 'Signup'}
                   </Button>
                 </Form>
               </>
