cf897945bc5e22182585d1a1958a4a19b3bbbe66
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-04-28 23:53:08 +0530

    Updated Sidebar component to use css to render drawer

--- src/components/dashboard/Sidebar/Sidebar.js
+++ src/components/dashboard/Sidebar/Sidebar.js
@@ -1,7 +1,6 @@
+import { memo } from 'react';
 import { FaArchive, FaTrash, FaHome, FaSignOutAlt } from 'react-icons/fa';
-import { useCallback } from 'react';
 import cn from 'classnames';
-import { useMediaQuery } from 'react-responsive';
 import { MdLabel } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
 import Button from 'components/common/Button/Button';
@@ -31,46 +30,22 @@ const linksProps = [
     name: 'Trash',
     icon: <FaTrash className="Sidebar__icon" />,
   },
-  // {
-  //   path: '/profile',
-  //   name: 'Profile',
-  //   icon: <FaUserCircle className="Sidebar__icon" />,
-  // },
 ];
 
 const Sidebar = () => {
   const { showDrawer, toggleDrawer } = useSideDrawer();
-  const xs = useMediaQuery({ maxWidth: '600px' });
 
-  const renderMobileDrawer = useCallback(
-    () => (
-      <div className="Sidebar__drawer">
-        {linksProps.map((linkObj) => (
-          <NavLink to={linkObj.path} key={linkObj.path} onClick={toggleDrawer}>
-            <div className="d-flex items-center Sidebar__drawer--link">
-              {linkObj.icon}
-              <p className="ml-2">{linkObj.name}</p>
-            </div>
-          </NavLink>
-        ))}
-        <Button component="div" className="my-2 content-start mx-1">
-          <Typography variant="h6" className="d-flex items-center" onClick={signout}>
-            <FaSignOutAlt className="mx-1" />
-            <span className="d-block">Debarshi</span>
-          </Typography>
-        </Button>
-      </div>
-    ),
-    [toggleDrawer]
-  );
-
-  return !xs ? (
-    <div className={cn('Sidebar__root')}>
+  return (
+    <div
+      className={cn('Sidebar__root', {
+        'Sidebar--slidein': showDrawer,
+      })}
+    >
       <nav className="d-flex flex-col content-between h-full">
         <div>
           {linksProps.map((linkObj) => (
             <NavLink to={linkObj.path} key={linkObj.path}>
-              <div className="Sidebar__link">
+              <div role="button" aria-hidden className="Sidebar__link" onClick={toggleDrawer}>
                 {linkObj.icon}
                 <p className="ml-2">{linkObj.name}</p>
               </div>
@@ -85,9 +60,7 @@ const Sidebar = () => {
         </Button>
       </nav>
     </div>
-  ) : (
-    showDrawer && renderMobileDrawer()
   );
 };
 
-export default Sidebar;
+export default memo(Sidebar);
