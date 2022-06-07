2adb5a13a33282f00f33cae8bd0913f4f7fcb63d
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-06-07 19:07:26 +0530

    Showing spinner on page refresh

--- src/components/common/PrivateRoute/PrivateRoute.js
+++ src/components/common/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,19 @@
+import BounceLoader from 'react-spinners/BounceLoader';
 import { useAuth } from 'providers/AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { colors } from 'styles/defaultStyles';
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
 
+  if (loading) {
+    return (
+      <div className="d-flex h-screen w-full content-center items-center">
+        <BounceLoader color={colors.colorPrimary} loading={loading} size={50} />
+      </div>
+    );
+  }
   if (user) {
     return children;
   }
--- src/providers/AuthProvider/AuthProvider.js
+++ src/providers/AuthProvider/AuthProvider.js
@@ -5,18 +5,24 @@ const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setAuthUser(user);
       } else {
         setAuthUser(null);
       }
+      setLoading(false);
     });
   }, []);
 
-  const value = useMemo(() => ({ user: authUser, authenticated: authUser !== null }), [authUser]);
+  const value = useMemo(
+    () => ({ user: authUser, loading, authenticated: authUser !== null }),
+    [authUser, loading]
+  );
 
   return <AuthContext.Provider value={value}>{children(authUser)}</AuthContext.Provider>;
 };
